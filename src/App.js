eeb283267df9029eb5ef1d0eed7f2be4d7c0ee7e
lynchycat21 <71978439+lynchycat21@users.noreply.github.com>
2021-02-06 12:57:05 +0100

    Took out any excess code that wasn't necessary

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <h1>
-          Weather App
+          
         </h1>
         <Weather city="Madrid" />
         <a href="https://github.com/lynchycat21/weather-react-app">Open source code</a> by Cathy Lynch.
-      </header>
     </div>
   );
 }
